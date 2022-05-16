060d04390240c2dbb40f994f687385ab7185ac6c
Ramesh Thrinadh Yatam <yrtrinadh@gmail.com>
2022-05-16 21:00:00 +0530

    Modified

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, redirect, render_template,request
 import requests, re
 
 app = Flask(__name__)
-count=0
 
-@app.route('/')
+def url():
+    URL="https://emojihub.herokuapp.com/api/random"
+    html=requests.get(url=URL)
+    data=html.json()
+    return data
 
+
+
+@app.route('/', methods=['GET','POST'])
 def index():
     htmlcode=""
     if request.method == "GET":
@@ -13,28 +19,39 @@ def index():
     return render_template('random.html',htmlcode=htmlcode)
 
 
+
+@app.route('/random', methods=['GET','POST'])
 def generate():
-    URL="https://emojihub.herokuapp.com/api/random"
-    html=requests.get(url=URL)
-    data=html.json()
+    data=url()
     value=data["htmlCode"]
     return (value[0])
+
+
 
 @app.route("/search/", methods=['GET',"POST"])
 def search():
     if request.method == 'POST':
-        URL="https://emojihub.herokuapp.com/api/all"
-        html=requests.get(url=URL)
-        data=html.json()
-        value=[]
-        search_word=request.form["search"]
-        for element in data:
-            if(re.search(search_word,element["name"])):
-                value.append(element["htmlCode"][0])
+        value=find(request.form["search"])
         return render_template('search.html',data=value)
     else:
-        return render_template("search.html")
+        value=find("")
+        return render_template("search.html",data=value)
 
+def find(search_word):
+    URL="https://emojihub.herokuapp.com/api/all"
+    html=requests.get(url=URL)
+    data=html.json()
+    value=[]
+    for element in data:
+        if(re.search(search_word,element["name"])):
+            value.append([element["name"],element["htmlCode"][0]])
+    return value
+
+
+@app.route('/results/', methods=['GET','POST'])
+def results():
+    value=find(request.form["search"])
+    return render_template("search.html",data=value)
 
 
 if __name__ =="__main__":
